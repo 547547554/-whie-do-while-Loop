0352550f4bfe57666cec17a158a63b98bfeceb13
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-11-19 19:32:43 +0530

    Add files via upload

---
+++ switch-case.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// {
+//     int i;
+//     cout<<"Enter the number : ";
+//     cin>>i;
+
+//     switch (i)
+//     {
+//     case 1:
+//         cout<<"Monday";
+//         break;
+
+//      case 2:
+//         cout<<"Tuesday";
+//         break;
+
+//      case 3:
+//         cout<<"Wednesday";
+//         break;
+
+//     case 4:
+//         cout<<"Thursday";
+//         break;
+    
+//     case 5:
+//         cout<<"Friday";
+//         break;
+    
+//     case 6:
+//         cout<<"Saturday";
+//         break;
+    
+//     case 7:
+//         cout<<"Sunday";
+//         break;
+
+//     default:
+//     cout<<"Enter valid day";
+//         break;
+//     }
+int arr[7] ={10,20,30,40};
+int n= sizeof(arr)/sizeof(arr[0]);
+cout<<n;
+}
