9902b2995bc71f2882db82bb8bcba6747f024a68
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-11-20 23:04:25 +0530

    Add files via upload

---
+++ while-loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 8;
+    int i = 1;  //intialize
+    while(i <= 12)  //break
+    {
+        cout<<i*n<<endl;
+        i++; //update
+        
+    }
+}
